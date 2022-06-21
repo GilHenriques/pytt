c4875fb163ed0128300c52e911b3643267713dac
Gil Henriques <gilhenris@gmail.com>
2022-06-21 17:00:20 +0200

    Document %+%

--- R/binary_operators.R
+++ R/binary_operators.R
@@ -1,1 +1,16 @@
-`%+%` <- function(a, b) paste0(a, b)
+#' Binary paste operator
+#'
+#' A binary version of the \code{\link[base]{paste0}} function.
+#'
+#' @param a An \code{R} object, to be converted to a character vector
+#' @param b An \code{R} object, to be converted to a character vector
+#'
+#' @return A character vector of the concatenated values.
+#' @export
+#'
+#' @examples
+#' 'I am ' %+% 31 %+% 'years old.'
+#'
+`%+%` <- function(a, b) {
+  paste0(a, b)
+}
